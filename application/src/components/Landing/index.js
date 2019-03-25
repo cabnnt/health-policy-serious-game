eaaea978f36981835b80259cd689fcdc1812d3c7
Christopher Bennett <cabnnt@users.noreply.github.com>
2019-03-24 20:42:04 -0400

    move form style into a styles folder to be re-used

--- application/src/components/Landing/index.js
+++ application/src/components/Landing/index.js
@@ -13,6 +13,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 
 import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
+import FormStyles from '../../styles/formStyles';
 
 const INITIAL_STATE = {
   signingIn: false,
@@ -21,37 +22,7 @@ const INITIAL_STATE = {
   error: null
 };
 
-const styles = theme => ({
-  main: {
-    width: 'auto',
-    display: 'block', // Fix IE 11 issue.
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
-      width: 400,
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    },
-  },
-  paper: {
-    marginTop: theme.spacing.unit * 8,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
-  },
-  avatar: {
-    margin: theme.spacing.unit,
-    backgroundColor: theme.palette.primary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing.unit,
-  },
-  submit: {
-    marginTop: theme.spacing.unit * 3,
-  },
-});
+const styles = FormStyles;
 
 class Landing extends Component {
   constructor(props) {
