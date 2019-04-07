87b62a8bc8d84a1ee9a23d94f7f04d366c03e987
Zach Lennane <zlennane@live.unc.edu>
2019-04-07 12:40:48 -0400

    admin stuff

---
+++ application/src/components/AdminPanel/index.js
@@ -0,0 +1,26 @@
+import TimeInput from 'material-ui-time-picker';
+import React from 'react';
+import { min } from 'moment';
+const moment = require('moment');
+export default class AdminPanel extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+      return <div>
+          <h2>AdminPanel</h2>
+          <TimeInput mode='12h' onChange={(time)=> this.handleChange(time)}/>
+      </div>
+    }
+    handleChange(params) {
+      let now = moment();
+      let till = moment(params);
+      console.log(`The moment now is : ${now}`);
+      console.log(`The moment till is : ${till}`);
+      var duration = moment.duration(till.diff(now));
+      var minutes = duration.asMinutes();
+      console.log(duration);
+      console.log(minutes);
+      
+    }
+  }
