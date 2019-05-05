2b42a42b9f6e8d56d363a646db18796869c739ab
Christopher Bennett <cabnnt@users.noreply.github.com>
2019-05-05 15:44:19 -0400

    fix current games list

--- application/src/components/SimpleTable/index.js
+++ application/src/components/SimpleTable/index.js
@@ -24,50 +24,48 @@ const SimpleTable = props => {
   const { classes, collection, attributes, headers } = props;
 
   return(
-    <Paper className={ classes.root }>
-      <Table className={ classes.table }>
-        <TableHead>
-          <TableRow>
-            { 
-              attributes.map((attribute, index) => {
-                return (
-                  index === 0
-                    ? <TableCell key={ `${attribute}-header` }>{ headers[attribute] }</TableCell>
-                    : <TableCell align='right'>{ headers[attribute] }</TableCell>
-                );
-              })
-            }
-            <TableCell />
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            collection.map(row => (
-              <TableRow key={ row.id }>
-                {
-                  attributes.map((attribute, index) => {
-                    return (
-                      index === 0
-                        ? <TableCell
-                            key={ row[attribute] }
-                            component='th'
-                            scope='row'
-                          >
-                            { row[attribute] }
-                          </TableCell>
-                        : <TableCell>{ row[attribute] }</TableCell>
-                    );
-                  })
-                }
-                <TableCell>
-                  <AuthorizedGameButtons gameId={ row.id } />
-                </TableCell>
-              </TableRow>
-            ))
+    <Table className={ classes.table }>
+      <TableHead>
+        <TableRow>
+          { 
+            attributes.map((attribute, index) => {
+              return (
+                index === 0
+                  ? <TableCell key={ `${attribute}-header` }>{ headers[attribute] }</TableCell>
+                  : <TableCell align='right'>{ headers[attribute] }</TableCell>
+              );
+            })
           }
-        </TableBody>
-      </Table>
-    </Paper>
+          <TableCell />
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {
+          collection.map(row => (
+            <TableRow key={ row.id }>
+              {
+                attributes.map((attribute, index) => {
+                  return (
+                    index === 0
+                      ? <TableCell
+                          key={ row[attribute] }
+                          component='th'
+                          scope='row'
+                        >
+                          { row[attribute] }
+                        </TableCell>
+                      : <TableCell>{ row[attribute] }</TableCell>
+                  );
+                })
+              }
+              <TableCell>
+                <AuthorizedGameButtons gameId={ row.id } />
+              </TableCell>
+            </TableRow>
+          ))
+        }
+      </TableBody>
+    </Table>
   )
 }
 
