4328922471965810d7dad37d915c2469bcc1662a
Christopher Bennett <cabnnt@users.noreply.github.com>
2019-05-07 10:00:34 -0400

    pretty up countdown timer

--- application/src/components/Countdown/index.js
+++ application/src/components/Countdown/index.js
@@ -5,6 +5,8 @@ import moment from 'moment'
 import Counter from 'react-countdown-now'
 import { withAuthorization } from '../Authorization/context';
 import {illmatic} from '../../js/stringGen'
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
 const INITIAL_STATE = {
     timeLeft : 0,
     startTime : 0,
@@ -14,7 +16,7 @@ const PRODUCTION_SECONDS = 60000
 const DEV_SECONDS = 1000
 const Complete = () => <span>You have completed the game </span>
 const renderer = ({ hours, minutes, seconds, completed }) => {
-    return <span>{hours}:{minutes}:{seconds}</span>;
+    return <Typography>{hours}:{minutes}:{seconds}</Typography>;
   };
 class Countdown extends React.Component{
     
@@ -46,8 +48,9 @@ class Countdown extends React.Component{
         })
     }
     render(){
+        const style = { margin: 10, maxWidth: 200, minHeight: 15 };
         //TODO: when time expires remove people from game in DB
-        return this.state.isStarted ? (<div>Time Remaining : <Counter date={this.state.startTime + this.state.timeLeft*PRODUCTION_SECONDS} renderer = {renderer}/></div>) : <span>Please wait until the game starts</span>;
+        return this.state.isStarted ? (<Paper style={ style }><Typography style={{ margin: '0px 5px' }}>Time remaining:</Typography><div style={{ width: '75%', margin: '0 auto' }}><Counter date={this.state.startTime + this.state.timeLeft*PRODUCTION_SECONDS} renderer={renderer}/></div></Paper>) : <Paper style={ style }>Please wait until the game starts</Paper>;
     }
 }
 export default withFirebase(Countdown);
